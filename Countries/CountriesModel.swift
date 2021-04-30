35eee9f5a48d5aed7cb60d6cb301f3aa10defc02
Alok <alokme123@gmail.com>
2021-04-30 14:56:03 +0530

    add files

---
+++ Countries/CountriesModel.swift
@@ -0,0 +1,13 @@
+//
+//  CountriesModel.swift
+//  Countries
+//
+//  Created by Alok N on 30/04/21.
+//
+
+struct Country{
+    let countryID: String
+    let countryName: String
+    let countryEmoji: String
+    let labelString: String
+}
---
+++ Countries/CountryCellView.swift
@@ -0,0 +1,15 @@
+//
+//  CountryCellView.swift
+//  Countries
+//
+//  Created by Alok N on 30/04/21.
+//
+
+import UIKit
+
+class CountryCellView: UITableViewCell {
+
+    //MARK: OUTLETS
+    @IBOutlet weak var countryNameLabel: UILabel!
+
+}
